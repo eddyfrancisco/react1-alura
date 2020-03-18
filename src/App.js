82197290a67531c9e2c05276c99b9de0f7eb449e
Eddy <ed@smartbpo.io>
2020-03-17 23:15:53 -0300

    Aula 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
 
 import './App.css';
+import Tabela from './Tabela';
 
 function App() {
+
+  const  autores =  [
+    {
+      nome: 'Paulo',
+      livro: 'React',
+      preco: '1000'
+    },
+    {
+      nome: 'Daniel',
+      livro: 'Java',
+      preco: '99'
+    },
+    {
+      nome: 'Marcos',
+      livro: 'Design',
+      preco: '150'
+    },
+    {
+      nome: 'Bruno',
+      livro: 'DevOps',
+      preco: '100'
+    }
+  ];
+
+
   return (
-  <h1>Hello to everyone</h1>
-  );
+   <div className="App">
+     <Tabela autores={ autores }/>
+   </div>
+    );
 }
 
 export default App;
